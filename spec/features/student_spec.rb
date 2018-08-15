e10b48501a64ae5d0aa46836b654aba3fa364d31
Jed Peek <jedpeek@yahoo.com>
2018-08-15 18:25:53 +0000

    automatically backed up by learn

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -29,12 +29,12 @@ describe 'Show page' do
   end
 
   it 'renders the first name in a h1 tag' do
-    visit student_path(@student)
+    visit students_path(@student)
     expect(page).to have_css("h1", text: "Daenerys")
   end
 
   it 'renders the last name in a h1 tag' do
-    visit student_path(@student)
+    visit students_path(@student)
     expect(page).to have_css("h1", text: "Targaryen")
   end
 
